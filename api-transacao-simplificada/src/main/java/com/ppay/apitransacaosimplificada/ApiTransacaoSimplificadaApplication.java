903df052d5e02735d374abe3153016989a65fca0
Karmaicom Rocha <karmaicom@gmail.com>
2026-07-06 16:12:35 -0300

    Commit inicial.

---
+++ api-transacao-simplificada/src/main/java/com/ppay/apitransacaosimplificada/ApiTransacaoSimplificadaApplication.java
@@ -0,0 +1,13 @@
+package com.ppay.apitransacaosimplificada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransacaoSimplificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransacaoSimplificadaApplication.class, args);
+	}
+
+}
---
+++ api-transacao-simplificada/src/test/java/com/ppay/apitransacaosimplificada/ApiTransacaoSimplificadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppay.apitransacaosimplificada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransacaoSimplificadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
