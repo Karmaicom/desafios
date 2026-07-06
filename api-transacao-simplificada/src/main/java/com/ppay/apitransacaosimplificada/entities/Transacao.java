efa63fed4a2aceab8ffa5e2e9dc3d3419dd24545
Karmaicom Rocha <karmaicom@gmail.com>
2026-07-06 19:40:12 -0300

    Atribuindo builder a transacao.

--- api-transacao-simplificada/src/main/java/com/ppay/apitransacaosimplificada/entities/Transacao.java
+++ api-transacao-simplificada/src/main/java/com/ppay/apitransacaosimplificada/entities/Transacao.java
@@ -2,12 +2,14 @@ package com.ppay.apitransacaosimplificada.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
